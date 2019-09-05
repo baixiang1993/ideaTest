1c3abcdda2a94ac24b0e884559c7ac8514067207
Bx <353193934@qq.com>
2019-09-05 10:03:53 +0800

    Initial commit

---
+++ javaweb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Bx
+  Date: 2019/9/4
+  Time: 16:54
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>111</title>
+  </head>
+  <body>
+  <h1 style="color: red">11111</h1>
+  </body>
+</html>
---
+++ src/com/travelsky/bean/Customer.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Travelsky Corp.
+ * All Rights Reserved.
+ */
+package com.travelsky.bean;
+
+import java.util.ArrayList;
+
+/**
+ * .
+ *
+ * @author BaiXiang 2019-09-04-13:48
+ * @version 1.0
+ */
+public class Customer {
+
+    public static void main(String[] args) {
+
+        ArrayList<Object> arr = new ArrayList<>();
+        for (Object o : arr) {
+            
+        }
+
+    }
+}
